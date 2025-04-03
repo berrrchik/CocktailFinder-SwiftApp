68e05afcf4aaaac7ce16353633496a63921ae22b
berrrchik <berrrchik@mail.ru>
2025-04-04 01:17:28 +0300

    Initial Commit

---
+++ CocktailFinder-SwiftApp/CocktailFinder_SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailFinder_SwiftAppApp.swift
+//  CocktailFinder-SwiftApp
+//
+//  Created by Анастасия Берчик on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CocktailFinder_SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocktailFinder-SwiftAppTests/CocktailFinder_SwiftAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailFinder_SwiftAppTests.swift
+//  CocktailFinder-SwiftAppTests
+//
+//  Created by Анастасия Берчик on 4/4/25.
+//
+
+import Testing
+@testable import CocktailFinder_SwiftApp
+
+struct CocktailFinder_SwiftAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
