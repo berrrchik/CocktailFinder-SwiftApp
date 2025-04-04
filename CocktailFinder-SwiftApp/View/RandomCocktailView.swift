07edd00a7560cd0883d05ed5c08e8385fa90724b
berrrchik <berrrchik@mail.ru>
2025-04-05 00:16:48 +0300

    feat: add Text and Button to RandomCoctailView

--- CocktailFinder-SwiftApp/View/RandomCocktailView.swift
+++ CocktailFinder-SwiftApp/View/RandomCocktailView.swift
@@ -1,7 +1,25 @@
-//
-//  RandomCocktailView.swift
-//  CocktailFinder-SwiftApp
-//
-//  Created by Анастасия Берчик on 4/4/25.
-//
+import SwiftUI
 
+struct RandomCocktailView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Случайный коктейль")
+                .font(.title)
+            Button {
+            }
+            label: {
+                Label("Попытать удачу", systemImage: "dice")
+            }
+            .font(.system(size: 18, weight: .semibold))
+            .foregroundColor(.white)
+            .padding(.vertical, 10)
+            .padding(.horizontal, 10)
+            .background(Color.blue)
+            .cornerRadius(14)
+        }
+    }
+}
+
+#Preview {
+    RandomCocktailView()
+}
