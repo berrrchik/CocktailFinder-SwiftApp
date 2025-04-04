67f13f9c6f143cd7c484018e391c8a51036238ed
berrrchik <berrrchik@mail.ru>
2025-04-05 00:11:05 +0300

    chore: rename FavoritesView to FavoriteCoctailsView

---
+++ CocktailFinder-SwiftApp/View/FavoriteCoctailsView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct FavoriteCoctailsView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Здесь будут ваши любимые коктейли")
+                .font(.title3)
+            Image(systemName: "heart.fill")
+                .font(.title)
+                .foregroundColor(.blue)
+        }
+    }
+}
+
+#Preview {
+    FavoriteCoctailsView()
+}
--- CocktailFinder-SwiftApp/View/FavoritesView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  FavoritesView.swift
-//  CocktailFinder-SwiftApp
-//
-//  Created by Анастасия Берчик on 4/4/25.
-//
-
