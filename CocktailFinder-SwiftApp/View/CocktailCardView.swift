b15f5d41a1f814a4717130cd0774cd72caa26ec0
berrrchik <berrrchik@mail.ru>
2025-04-08 14:37:36 +0300

    feat: update padding for better view

--- CocktailFinder-SwiftApp/View/CocktailCardView.swift
+++ CocktailFinder-SwiftApp/View/CocktailCardView.swift
@@ -6,7 +6,6 @@ struct CocktailCardView: View {
     
     var body: some View {
         VStack(spacing: 8) {
-            // Фото коктейля
             WebImage(url: URL(string: cocktail.thumbnail))
                 .resizable()
                 .indicator(.activity)
@@ -17,13 +16,11 @@ struct CocktailCardView: View {
                 .cornerRadius(4)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
             
-            // Название коктейля
             Text(cocktail.name)
                 .font(.system(size: 22, weight: .medium))
                 .lineLimit(1)
                 .frame(width: 160, alignment: .center)
             
-            // Кнопка "Рецепт"
             NavigationLink(destination: RecipeView(drinkId: cocktail.id)) {
                 Text("Рецепт")
                     .font(.system(size: 16, weight: .medium))
@@ -34,20 +31,18 @@ struct CocktailCardView: View {
             }
             .buttonStyle(PlainButtonStyle())
         }
-        .padding()
+        .padding(10)
         .background(Color.gray.opacity(0.2))
         .cornerRadius(15)
     }
 }
 
-// Превью для CocktailCardView с загрузкой реальных данных
 struct CocktailCardView_Previews: PreviewProvider {
     static var previews: some View {
         CocktailCardPreview()
     }
 }
 
-// Вспомогательное превью с загрузкой данных
 struct CocktailCardPreview: View {
     @State private var cocktail: Cocktail?
     @State private var isLoading = false
@@ -106,30 +101,3 @@ struct CocktailCardPreview: View {
     }
 }
 
-// Вспомогательный класс для создания моковых данных
-//struct MockData {
-//    static var sampleCocktail: Cocktail {
-//        // Создаем ингредиенты Маргариты
-//        let ingredients = [
-//            Cocktail.Ingredient(name: "Tequila", measure: "1 1/2 oz"),
-//            Cocktail.Ingredient(name: "Triple sec", measure: "1/2 oz"),
-//            Cocktail.Ingredient(name: "Lime juice", measure: "1 oz"),
-//            Cocktail.Ingredient(name: "Salt", measure: "")
-//        ]
-//        
-//        // Используем конструктор, который точно соответствует модели данных
-//        return Cocktail(
-//            id: "11007",
-//            name: "Margarita",
-//            tags: "IBA,ContemporaryClassic",
-//            category: "Ordinary Drink",
-//            alcoholic: "Alcoholic",
-//            glass: "Cocktail glass",
-//            instructions: "Rub the rim of the glass with the lime slice to make the salt stick to it.",
-//            thumbnail: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
-//            ingredients: ingredients,
-//            isFavorite: false
-//        )
-//    }
-//}
-//
